21af15081d735069ea5f0c75257ebc46b1e35bc2
FUJI Goro <goro@fastly.com>
2024-07-05 08:36:20 +0900

    implement titys2json with the TypeScript compiler

---
+++ titys2json/index.mjs
@@ -0,0 +1,65 @@
+#!/usr/bin/env node
+import * as fs from "node:fs";
+import * as path from "node:path";
+import * as ts from "typescript";
+
+function die(msg) {
+  console.error(msg);
+  process.exit(1);
+}
+
+const tsFile = process.argv[2] ?? die("No file provided.");
+const TMPDIR = process.env.TMPDIR ?? die("No TMPDIR provided.");
+const jsFile = `${TMPDIR}/${path.basename(tsFile).replace(/\.m?tsx?$/, ".js")}`;
+const mjsFile = `${TMPDIR}/${path.basename(tsFile).replace(/\.m?tsx?$/, ".mjs")}`;
+process.on("exit", () => {
+  for (const file of [jsFile, mjsFile]) {
+    if (fs.existsSync(file)) {
+      fs.unlinkSync(file);
+    }
+  }
+});
+
+// load the typescript file
+const program = ts.createProgram([tsFile], {
+  strict: true,
+  noEmitOnError: true,
+  target: ts.ScriptTarget.ESNext,
+  module: ts.ModuleKind.ESNext,
+  outDir: TMPDIR,
+});
+
+let emitResult = program.emit();
+
+let allDiagnostics = ts
+  .getPreEmitDiagnostics(program)
+  .concat(emitResult.diagnostics);
+
+const uniq = new Set();
+for (const diagnostic of allDiagnostics) {
+  if (uniq.has(diagnostic.code)) {
+    continue;
+  }
+  uniq.add(diagnostic.code);
+
+  if (diagnostic.file) {
+    let { line, character } = ts.getLineAndCharacterOfPosition(
+      diagnostic.file,
+      diagnostic.start
+    );
+    let message = ts.flattenDiagnosticMessageText(diagnostic.messageText, "\n");
+    console.error(
+      `${diagnostic.file.fileName}:${line + 1}:${character + 1}: ${message}`
+    );
+  } else {
+    console.error(ts.flattenDiagnosticMessageText(diagnostic.messageText, "\n"));
+  }
+}
+
+if (emitResult.emitSkipped) {
+  process.exit(1);
+} else {
+  fs.renameSync(jsFile, mjsFile);
+  const result = await import(mjsFile);
+  console.log(JSON.stringify(result, null, 2));
+}
