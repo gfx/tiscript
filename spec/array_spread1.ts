4b6babbab98a95dd74231293a7f89fb81c9a4ed1
FUJI Goro <goro@fastly.com>
2024-07-09 15:20:01 +0900

    add tests

---
+++ spec/array_spread1.ts
@@ -0,0 +1,2 @@
+const arr1 = [1, 2];
+export const array = [100, 200, ...arr1];
---
+++ spec/array_spread3.ts
@@ -0,0 +1,4 @@
+const arr1 = [1, 2];
+const arr2 = [3, 4];
+const arr3 = [5, 6];
+export const array = [...arr1, ...arr2, ...arr3];
